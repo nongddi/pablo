4c5dd2942d5f90e031d654b8c3f8ea4e9b649382
nongddi <nongddiverse@gmail.com>
2022-06-11 12:35:40 +0900

    crawling code test

--- pablo/keywords/crawling.py
+++ pablo/keywords/crawling.py
@@ -1,4 +1,9 @@
 #%%
-print("Hello World!!")
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
 
 #%%
+driver = webdriver.Chrome('C:/Users/segre/Desktop/project/pablo/chromedriver_win32/chromedriver.exe')
+base_url = 'https://cafe.naver.com/ArticleList.nhn?search.clubid=29890867&search.menuid=1&search.boardtype=L&search.totalCount=151&search.cafeId=29890867&search.page=1'
+driver.get(base_url)
