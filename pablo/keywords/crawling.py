0dea30e01b7e97fb7999312290095c2dd499a33c
nongddi <nongddiverse@gmail.com>
2022-06-12 11:54:20 +0900

    crawling code

--- pablo/keywords/crawling.py
+++ pablo/keywords/crawling.py
@@ -1,5 +1,6 @@
 #%%
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 
@@ -7,3 +8,11 @@ import time
 driver = webdriver.Chrome('C:/Users/segre/Desktop/project/pablo/chromedriver_win32/chromedriver.exe')
 base_url = 'https://cafe.naver.com/ArticleList.nhn?search.clubid=29890867&search.menuid=1&search.boardtype=L&search.totalCount=151&search.cafeId=29890867&search.page=1'
 driver.get(base_url)
+
+post_list = driver.find_elements(by=By.TAG_NAME, value='tbody')
+print(post_list)
+
+for post in post_list:
+    href = post.get_attribute('href')
+    print(href)
+# %%
